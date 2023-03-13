1a67d52bafeeebe69f98f3ac0e0732f356d92f1d
Vitalii Khymchuk <vitalii.workbox001@gmail.com>
2023-03-13 13:47:02 +0200

    changed tests

--- __tests__/signUpControllerTest.js
+++ __tests__/signUpControllerTest.js
@@ -3,10 +3,14 @@ const { User } = require("../src/models");
 const bcrypt = require("bcrypt");
 const { HttpError } = require("../src/utils");
 const { sendGrid } = require("../src/services");
+jest.mock("uuid", () => ({ v4: jest.fn(() => "testRandomId") }));
 require("dotenv").config();
 
 describe("Sign Up controller test", () => {
-  it("should crete new user, write to res token and status code ", async () => {
+  const hashedPW = "hashedPassword";
+  const mNext = jest.fn();
+
+  it("should to crete new user in case if user doesnt exist, send verification link, write to res message and status code ", async () => {
     let mUser = {};
     const mReq = {
       body: { email: "test", name: "test", password: "test" },
@@ -29,7 +33,7 @@ describe("Sign Up controller test", () => {
       .mockImplementationOnce(() => Promise.resolve(null));
 
     jest.spyOn(User, "create").mockImplementationOnce((data) => {
-      const newUser = { ...data, _id: "123" };
+      const newUser = { ...data, _id: "123", verify: false };
       mUser = newUser;
       return Promise.resolve(newUser);
     });
@@ -41,21 +45,86 @@ describe("Sign Up controller test", () => {
 
     jest
       .spyOn(bcrypt, "hash")
-      .mockImplementationOnce(() => Promise.resolve("hashedPassword"));
+      .mockImplementationOnce(() => Promise.resolve(hashedPW));
 
     const mSendGrid = jest
       .spyOn(sendGrid, "sendVerificationEmail")
       .mockImplementationOnce(() => Promise.resolve());
 
-    const mNext = jest.fn();
-
     await signup(mReq, mRes, mNext);
-    expect(mSendGrid).toHaveBeenCalledWith(mReq.body.email);
+    //check if verification email has been sent
+    expect(mSendGrid).toHaveBeenCalledWith(
+      mReq.body.email,
+      `http://${mReq.headers.host}/api/users/verify/testRandomId`
+    );
     //check user fields
     expect(mUser.email).toBe(mReq.body.email);
     expect(mUser.name).toBe(mReq.body.name);
     expect(mUser.password).toBe("hashedPassword");
     expect(mUser._id).toBe("123");
+    expect(mUser.verify).toBe(false);
+    expect(typeof mUser.avatar.imageLink).toBe("string");
+    expect(typeof mUser.avatar.name).toBe("string");
+    //check res
+    expect(mRes.code).toBe(201);
+    expect(mRes.data.code).toBe(201);
+    expect(mRes.data.message).toBe(
+      `Profile has been created, verification link were sent to ${mUser.email}`
+    );
+  });
+  it("should to update user data in case if user already exist but not verified, send verification link, write to res message and status code ", async () => {
+    let mUser = { verify: true, _id: "123" };
+    const mReq = {
+      body: { email: "test", name: "test", password: "test" },
+      headers: { host: "test" },
+    };
+    const mRes = {
+      code: null,
+      data: null,
+      status(code) {
+        this.code = code;
+        return { json: this.json.bind(this) };
+      },
+      json(data) {
+        this.data = data;
+      },
+    };
+
+    jest
+      .spyOn(User, "findOne")
+      .mockImplementationOnce(() => Promise.resolve(null));
+
+    jest.spyOn(User, "create").mockImplementationOnce((data) => {
+      const newUser = { ...data, _id: "123", verify: false };
+      mUser = newUser;
+      return Promise.resolve(newUser);
+    });
+
+    jest.spyOn(User, "findByIdAndUpdate").mockImplementationOnce((id, data) => {
+      mUser = { ...mUser, ...data };
+      return Promise.resolve(mUser);
+    });
+
+    jest
+      .spyOn(bcrypt, "hash")
+      .mockImplementationOnce(() => Promise.resolve(hashedPW));
+
+    const mSendGrid = jest
+      .spyOn(sendGrid, "sendVerificationEmail")
+      .mockImplementationOnce(() => Promise.resolve());
+
+    await signup(mReq, mRes, mNext);
+    //check if verification email has been sent
+    expect(mSendGrid).toHaveBeenCalledWith(
+      mReq.body.email,
+      `http://${mReq.headers.host}/api/users/verify/testRandomId`
+    );
+    //check user fields
+    expect(mUser.email).toBe(mReq.body.email);
+    expect(mUser.name).toBe(mReq.body.name);
+    expect(mUser.password).toBe("hashedPassword");
+    expect(mUser._id).toBe("123");
+    expect(mUser.verify).toBe(false);
     expect(typeof mUser.avatar.imageLink).toBe("string");
     expect(typeof mUser.avatar.name).toBe("string");
     //check res
